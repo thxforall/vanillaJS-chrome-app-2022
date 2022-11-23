df6b783d4d8378761550ef14bb007091e2de61a0
thxforall <brlduf@naver.com>
2022-11-23 10:28:24 +0900

    1123

--- momentum/app.js
+++ momentum/app.js
@@ -1,5 +1,11 @@
-const week = ["mon", "tue", "wed", "thu", "fir", "sat"];
+const age = parseInt(prompt("How old are you?"));
 
-console.log(week);
-
-week.push("sun");
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age < 20) {
+  console.log("You can't drink soju");
+} else if (age >= 20 && age <= 50) {
+  console.log("Let's go Soju");
+} else {
+  console.log("Beep");
+}
