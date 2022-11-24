cec472df38c731cf7e81efc14da8f09dee4204ce
thxforall <brlduf@naver.com>
2022-11-24 11:50:40 +0900

    1124

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,14 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector("div.hello:first-child h1");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number");
-} else if (age < 20) {
-  console.log("You can't drink soju");
-} else if (age >= 20 && age <= 50) {
-  console.log("Let's go Soju");
-} else {
-  console.log("Beep");
+function handleTitleClick() {
+  const currentColor = title.style.color;
+  let newColor;
+  if (currentColor === "tomato") {
+    newColor = "blue";
+  } else {
+    newColor = "tomato";
+  }
+  title.style.color = newColor;
 }
+
+title.addEventListener("click", handleTitleClick);
