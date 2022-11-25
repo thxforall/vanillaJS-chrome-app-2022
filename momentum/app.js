e5f1c0a4e591d5108205abbc1b26f912752b634e
thxforall <brlduf@naver.com>
2022-11-25 21:16:28 +0900

    1125

--- momentum/app.js
+++ momentum/app.js
@@ -1,14 +1,7 @@
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = title.style.color;
-  let newColor;
-  if (currentColor === "tomato") {
-    newColor = "blue";
-  } else {
-    newColor = "tomato";
-  }
-  title.style.color = newColor;
+  title.classList.toggle("clicked");
 }
 
 title.addEventListener("click", handleTitleClick);
