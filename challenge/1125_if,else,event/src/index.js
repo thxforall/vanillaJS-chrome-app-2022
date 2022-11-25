9946e0216294526c863457060db35470aa1cb8aa
thxforall <brlduf@naver.com>
2022-11-26 03:08:09 +0900

    11126

--- challenge/1125_if,else,event/src/index.js
+++ challenge/1125_if,else,event/src/index.js
@@ -1,9 +1,22 @@
 const background = document.body;
-let screenSize = window.innerWidth;
 
 function handleWindowResize() {
-  if (screenSize <= 300) {
-    background.className = "green";
+  let screenSize = window.innerWidth;
+  if (screenSize <= 200) {
+    background.classList.add("green");
+    background.classList.remove("yellow");
+  } else if (screenSize > 200 && screenSize <= 400) {
+    background.classList.remove("green");
+    background.classList.add("yellow");
+    background.classList.remove("peach");
+  } else if (screenSize > 400 && screenSize <= 600) {
+    background.classList.remove("yellow");
+    background.classList.add("peach");
+    background.classList.remove("orange");
+  } else if (screenSize > 600 && screenSize <= 800) {
+    background.classList.remove("peach");
+    background.classList.add("orange");
+  }
 }
 
 window.addEventListener("resize", handleWindowResize);
