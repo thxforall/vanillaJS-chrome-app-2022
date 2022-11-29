39d84f6afe04fefe4d178bd73d23018a32d86821
thxforall <brlduf@naver.com>
2022-11-29 21:42:00 +0900

    5.1 Clock interval

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -8,14 +8,14 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem("USERNAME_KEY", username);
-  paintGreetings(username);
+  localStorage.setItem("USERNAME_KEY", loginInput.value);
+  paintGreetings();
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem("USERNAME_KEY");
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -26,5 +26,5 @@ if (savedUsernmae === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings(savedUsernmae);
+  paintGreetings();
 }
