40de93adb3e9aa4fab94c2048c783cf9da8c1d1f
thxforall <brlduf@naver.com>
2022-11-30 21:38:08 +0900

    quote.js

---
+++ momentum/js/quotes.js
@@ -0,0 +1,52 @@
+const quotes = [
+  {
+    quote:
+      "풍파는 언제나 전진하는 자의 벗이다. 풍파 없는 항해는 얼마나 단조로운가. 고난이 심할수록 나의가슴은 고동친다",
+    author: "Friedrich Nietzsche",
+  },
+  {
+    quote: "중요한건 꺾이지 않는 마음",
+    author: "Deft",
+  },
+  {
+    quote: "그럼에도 불구하고",
+    author: "BKY",
+  },
+  {
+    quote: "가장 중요한 것은 눈에 보이지 않아",
+    author: "어린왕자",
+  },
+  {
+    quote: "오히려 좋아",
+    author: "미상",
+  },
+  {
+    quote: "Thx for all",
+    author: "BKY",
+  },
+  {
+    quote:
+      "The only Thing I do know is that we have to be kind. Please. be kind",
+    author: "Everything Everywhere All At Once",
+  },
+  {
+    quote: "I'm gonna live every minute of it",
+    author: "Soul",
+  },
+  {
+    quote: "I am Iron man",
+    author: "Ironman",
+  },
+  {
+    quote: "I thougt my life was a tragedy, but it was a fucking comedy",
+    author: "Joker",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
