57cbf14efce2d1de1110ddd2b8a99a4496639a63
thxforall <brlduf@naver.com>
2022-12-04 14:34:54 +0900

    js course

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,21 +2,26 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+let toDos = [];
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
   const targetLi = event.target.parentElement;
   targetLi.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(targetLi.id));
+  saveToDos();
 }
 
-function paintToDo(newTodo) {
+function paintToDo(newTodDoObj) {
   const li = document.createElement("li");
+  li.id = newTodDoObj.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodDoObj.text;
   const btn = document.createElement("button");
   btn.innerText = "❌";
   btn.addEventListener("click", deleteToDo);
@@ -27,11 +32,22 @@ function paintToDo(newTodo) {
 
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newTodo = toDoInput.value;
+  const newToDo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newTodDoObj);
+  paintToDo(newTodDoObj);
   saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const getToDos = localStorage.getItem(TODOS_KEY);
+if (getToDos !== null) {
+  const parsedToDos = JSON.parse(getToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach((item) => paintToDo(item));
+}
---
+++ momentum/js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "2547177bef58bd74709da57c9f14fa33";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    })
+  );
+}
+function onGeoErr() {
+  alert("Can't find youo. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErr);
